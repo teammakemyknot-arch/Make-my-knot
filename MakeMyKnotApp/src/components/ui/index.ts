3efdc0d422949b643924ca059372342e460db909
teammakemyknot-arch <team.makemyknot@gmail.com>
2025-09-04 18:52:15 +0000

    update something

--- MakeMyKnotApp/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Card } from './Card';
-export { default as Modal } from './Modal';
-
-export type { ButtonProps } from './Button';
-export type { InputProps } from './Input';
-export type { CardProps } from './Card';
-export type { ModalProps } from './Modal';
